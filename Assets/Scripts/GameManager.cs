c47a7ce7360875a56466f57f6e7831d831a0ce4b
Portix <92767862+Portix-S@users.noreply.github.com>
2024-03-26 23:01:41 -0300

    Death Screen

--- Assets/Scripts/GameManager.cs
+++ Assets/Scripts/GameManager.cs
@@ -1,3 +1,4 @@
+using System;
 using System.Collections;
 using System.Collections.Generic;
 using TMPro;
@@ -6,16 +7,33 @@ using UnityEngine.UI;
 
 public class GameManager : MonoBehaviour
 {
+    public static GameManager Instance;
+    
     [SerializeField] private GameObject spawner;
     [SerializeField] private GameObject player;
     [SerializeField] private GameObject mainMenu;
+    [SerializeField] private GameObject deathMenu;
 
     [SerializeField] private TextMeshProUGUI playerName;
+    [SerializeField] private TextMeshProUGUI deathMenuScoreText;
 
     [SerializeField] private GameObject errorOnInputField;
     private bool validName;
+    
+    private float timeSurvived;
+    private bool isGameRunning;
+
+    private void Start()
+    {
+        if(Instance == null)
+            Instance = this;
+        else
+            Destroy(this.gameObject);
+    }
+
     public void StartGame()
     {
+        timeSurvived = 0f;
         if (!validName)
         {
             ShowError();
@@ -23,11 +41,38 @@ public class GameManager : MonoBehaviour
         }
         
         mainMenu.SetActive(false);
+        deathMenu.SetActive(false);
         spawner.SetActive(true);
         player.SetActive(true);
+        isGameRunning = true;
+    }
+
+    private void Update()
+    {
+        if(isGameRunning)
+            timeSurvived += Time.deltaTime;
+    }
+
+
+    public void ShowMainMenu()
+    {
+        mainMenu.SetActive(true);
+        spawner.SetActive(false);
+        player.SetActive(false);
+        deathMenu.SetActive(false);
     }
     
-
+    public void ShowDeathMenu(string playerName)
+    {
+        isGameRunning = false;
+        // Show time survived with only 2 decimal places
+        deathMenuScoreText.text = playerName + " sobreviveu por " + timeSurvived.ToString("F2") + " segundos";
+        deathMenu.SetActive(true);
+        spawner.SetActive(false);
+        player.SetActive(false);
+        mainMenu.SetActive(false);
+    }
+    
     public void CheckIfValidName(TMP_InputField inputField)
     {
         if(inputField.text.Length is < 3 or > 10)
